4ddd7fb989cd742ae0e00460f70e4c15caac7c86
Kyle Nakamura <kylenakamura@kyles-macbook-pro.local>
2016-08-09 00:17:11 -0700

    Working on Dining Plan VC

---
+++ Open Den/DiningPointsVC.swift
@@ -0,0 +1,36 @@
+//
+//  DiningPointsVC.swift
+//  Hungry Cougar 2.0
+//
+//  Created by Kyle Nakamura on 8/9/16.
+//  Copyright © 2016 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class DiningPointsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Variables
+    var diningPlansArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+}
