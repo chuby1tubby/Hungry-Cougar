35fc8b9f9825d6102530162fde3ff7a1b637eba1
Kyle Nakamura <kylenakamura@kyles-macbook-pro.local>
2016-08-09 10:53:25 -0700

    Calculating Dining Points

--- Open Den/DiningPlanVC.swift
+++ Open Den/DiningPlanVC.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class DiningPlanVC: UIViewController {
 
+    // Outlets
     @IBOutlet weak var diningPlanLbl: UILabel!
+    
+    // Variables
+    var currentPoints = 0.0
+    var mealBudget = 0.0
+    var weeklyBudget = 0.0
+    
+    // Constants
+    let mealBudgets: [Double] = [1162, 978, 696, 554, 363]
+    let weeklyBudgets: [Double] = [72.5, 61, 43.5, 35, 22.5]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewWillLayoutSubviews() {
+    func calculateDiningPoints() {
+        switch diningPlanChoice {
+        case "No Worries":
+            mealBudget = mealBudgets[0]
+            weeklyBudget = weeklyBudgets[0]
+        case "We've Got You Covered":
+            mealBudget = mealBudgets[1]
+            weeklyBudget = weeklyBudgets[1]
+        case "Weekend Away":
+            mealBudget = mealBudgets[2]
+            weeklyBudget = weeklyBudgets[2]
+        case "Forgot To Cook":
+            mealBudget = mealBudgets[3]
+            weeklyBudget = weeklyBudgets[3]
+        case "Grab And Go":
+            mealBudget = mealBudgets[4]
+            weeklyBudget = weeklyBudgets[4]
+        default:
+            mealBudget = mealBudgets[5]
+            weeklyBudget = weeklyBudgets[5]
+        }
     }
 }
